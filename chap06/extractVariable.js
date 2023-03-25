3268f25609e507c7c28fd18750eead8d0d163194
witchicken <witchicken2@gmail.com>
2023-03-25 20:48:11 +0900

    refactor : extract variable

--- chap06/extractVariable.js
+++ chap06/extractVariable.js
@@ -1,11 +1,9 @@
-//1
+//3
 function price(order) {
   //가격(price) = 기본 가격 - 수량할인 + 배송비
-  return (
-    order.quantity * order.itemPrice -
-    //500개보다 많을때 할인
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    //100개보다 적을때 할인
-    Math.min(order.quantity * order.itemPrice * 0.1, 100)
-  );
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100); //배송비비
+  return basePrice - quantityDiscount + shipping;
 }
